19edcb6245dccaff74da975ccaafa5adebdc15a4
synthia <synthia.han@onja.org>
2020-09-30 08:32:26 +0300

    fix the date

--- script.js
+++ script.js
@@ -24,18 +24,77 @@ async function getData() {
         const newDataSort = people.sort((a, b) => b.birthday - a.birthday);
         //created html and  map the newDataSort.
         const html = newDataSort.map((person, index) => {
+
+            // function getAge(dateString) {
+            //     var today = new Date();
+            //     var DOB = new Date(dateString);
+            //     var totalMonths = (today.getFullYear() - DOB.getFullYear()) * 12 + today.getMonth() - DOB.getMonth();
+            //     totalMonths += today.getDay() < DOB.getDay() ? -1 : 0;
+            //     var years = today.getFullYear() - DOB.getFullYear();
+            //     if (DOB.getMonth() > today.getMonth())
+            //         years = years - 1;
+            //     else if (DOB.getMonth() === today.getMonth())
+            //         if (DOB.getDate() > today.getDate())
+            //             years = years - 1;
+
+            //     var days;
+            //     var months;
+
+            //     if (DOB.getDate() > today.getDate()) {
+            //         months = (totalMonths % 12);
+            //         if (months == 0)
+            //             months = 11;
+            //         var x = today.getMonth();
+            //         switch (x) {
+            //             case 1:
+            //             case 3:
+            //             case 5:
+            //             case 7:
+            //             case 8:
+            //             case 10:
+            //             case 12: {
+            //                 var a = DOB.getDate() - today.getDate();
+            //                 days = 31 - a;
+            //                 break;
+            //             }
+            //             default: {
+            //                 var a = DOB.getDate() - today.getDate();
+            //                 days = 30 - a;
+            //                 break;
+            //             }
+            //         }
+
+            //     }
+            //     else {
+            //         days = today.getDate() - DOB.getDate();
+            //         if (DOB.getMonth() === today.getMonth())
+            //             months = (totalMonths % 12);
+            //         else
+            //             months = (totalMonths % 12) + 1;
+            //     }
+            //     var age = years + ' years ' + months + ' months ' + days + ' days';
+            //     return age;
+            // }
+            
             function calculate_age(dob) { 
                 var diff_ms = Date.now() - dob.getTime();
                 var age_dt = new Date(diff_ms); 
                 return Math.abs(age_dt.getUTCFullYear() - 1970);
             }
-            
             const year = calculate_age(new Date(person.birthday));
-        return `
+            const newDate = new Date(person.birthday);
+            const month = newDate.toLocaleString('default', { month: 'long' });
+            const dayBirthday = newDate.getDate();
+            // function calculate_month (month){
+            //     var diff_ms = Date.now() - month.getTime();
+            //     var month_dt = new Date(diff_ms); 
+            //     return Math.abs(age_dt.)
+            // }
+            return `
     <tr data-id="${person.id}" class="${index % 2 ? 'even' : ''}">
       <td><img src="${person.picture}" alt="${person.firstName + ' ' + person.lastName}"/>      </td>
       <td>${person.lastName} ${person.firstName}
-         <p>Turns ${year} year on  </p>
+         <p>Turns ${year} on ${month} on ${dayBirthday} th </p>
       </td>
       <td>${person.birthday}
       </td>
